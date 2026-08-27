4ca2b3efa0f1421848a41ce06c3c88e024f796cc
santiago <szambranom@cuc.edu.co>
2026-08-26 23:10:38 -0500

    refactor: Correcion en el nombre de variable -btnModo-

--- back.js
+++ back.js
@@ -1,6 +1,6 @@
-const btnmodo = document.querySelector('#togglemodo');
+const btnModo = document.querySelector('#togglemodo');
 
-  btnmodo.onclick = function() {
+  btnModo.onclick = function() {
     document.body.classList.toggle('modo-claro')
 
     const modoactual = document.body.classList.contains('modo-claro') ? 'claro' : 'oscuro';
@@ -9,6 +9,6 @@ const btnmodo = document.querySelector('#togglemodo');
 
     const svgLuna = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="#000000"><path d="M12 11.807A9.002 9.002 0 0 1 10.049 2a9.942 9.942 0 0 0-5.12 2.735c-3.905 3.905-3.905 10.237 0 14.142 3.906 3.906 10.237 3.905 14.143 0a9.946 9.946 0 0 0 2.735-5.119A9.003 9.003 0 0 1 12 11.807z" fill="#000000"/></svg>'
 
-    btnmodo.innerHTML=modoactual==='claro' ? svgSol : svgLuna;
+    btnModo.innerHTML=modoactual==='claro' ? svgSol : svgLuna;
 
 }
